92a58d8db5828001c82520a666732a141972117d
Savochkin Oleg <savochkinoleg17@gmail.com>
2024-11-21 19:06:41 +0200

    fixed pop-up error

--- frontend/src/components/common/pop-up/PopUp.jsx
+++ frontend/src/components/common/pop-up/PopUp.jsx
@@ -2,10 +2,15 @@ import "./PopUp.css"
 
 const PopUp = ({active, setActive, children, errors}) => {
 
-    const errorsIsEmpty = Object.keys(errors).length > 0;
+    const errorsIsEmpty = errors && Object.keys(errors).length > 0;
 
-    const classNameIfErrors = errorsIsEmpty ? "pop-up__content pop-up__content--errors" : "pop-up__content";
-    const classNameIsActive = active ? "pop-up__content active" : "pop-up__content";
+    const classNameIfErrors = errorsIsEmpty
+        ? "pop-up__content pop-up__content--errors"
+        : "pop-up__content";
+
+    const classNameIsActive = active
+        ? "pop-up__content active"
+        : "pop-up__content";
 
     return <section className={active ? "pop-up active" : "pop-up"} onClick={() => setActive(false)}>
         <div className={classNameIfErrors + ' ' + classNameIsActive} onClick={(e) => e.stopPropagation()}>
