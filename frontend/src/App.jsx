b60997cefeba4ecb127ba2cc1e55b4a1d3fd6e8a
Dev1NA <andreykoandroshchuk@gmail.com>
2024-12-08 15:41:36 +0200

    fix: show spinner exclusively without header during loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {
-    Route,
-    Routes
+  Route,
+  Routes
 } from "react-router-dom";
 
 import Header from "./components/common/header/Header";
@@ -14,117 +14,120 @@ import ServicesPage from "./components/services-page/ServicesPage";
 import MediaPage from "./components/media-page/MediaPage";
 import VacanciesListPage from "./components/vacancies-list-page/VacanciesListPage";
 import VacancyDetailPage from "./components/vacancy-detail-page/VacancyDetailPage";
-import {useEffect, useState} from "react";
-import {LanguageContext, getLangFromLocaleStorage} from "./translations/language";
+import { useEffect, useState } from "react";
+import { LanguageContext, getLangFromLocaleStorage } from "./translations/language";
 import ScrollToTop from "./components/main-page/scroll-to-top/ScrollToTop";
 import useAPIClient from "./hooks/api.hook";
 import useHttp from "./hooks/http.hook";
 import Spinner from "./components/common/spinner/Spinner";
-import {shuffle} from "./utils/arrays";
+import { shuffle } from "./utils/arrays";
 
 
 function App() {
 
-    const [language, setLanguage] = useState(getLangFromLocaleStorage())
-    const client = useAPIClient(language);
+  const [language, setLanguage] = useState(getLangFromLocaleStorage())
+  const client = useAPIClient(language);
 
-    const {request: getContacts} = useHttp(client.getContacts);
-    const {request: getPhones} = useHttp(client.getPhoneNumbers);
-    const {request: getAboutUs} = useHttp(client.getAboutUs);
-    const {request: getServices} = useHttp(client.getServices);
-    const {request: getReviews, loading: loadingReviews} = useHttp(client.getReviews);
-    const {request: getNews, loading: loadingNews} = useHttp(client.getNews);
-    const {request: getVacancies, loading: loadingVacancies} = useHttp(client.getVacancies);
-    const {request: getMediaImages, loading: loadingMediaImages} = useHttp(client.getMediaImages);
-    const {request: getMediaVideos, loading: loadingMediaVideos} = useHttp(client.getMediaVideos);
+  const { request: getContacts } = useHttp(client.getContacts);
+  const { request: getPhones } = useHttp(client.getPhoneNumbers);
+  const { request: getAboutUs } = useHttp(client.getAboutUs);
+  const { request: getServices } = useHttp(client.getServices);
+  const { request: getReviews, loading: loadingReviews } = useHttp(client.getReviews);
+  const { request: getNews, loading: loadingNews } = useHttp(client.getNews);
+  const { request: getVacancies, loading: loadingVacancies } = useHttp(client.getVacancies);
+  const { request: getMediaImages, loading: loadingMediaImages } = useHttp(client.getMediaImages);
+  const { request: getMediaVideos, loading: loadingMediaVideos } = useHttp(client.getMediaVideos);
 
-    const [isFetchError, setIsFetchError] = useState(false);
-    const [loading, setLoading] = useState(true);
+  const [isFetchError, setIsFetchError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    const [contacts, setContacts] = useState({});
-    const [aboutUs, setAboutUs] = useState({});
-    const [services, setServices] = useState([]);
-    const [reviewsData, setReviewsData] = useState([]);
-    const [newsData, setNewsData] = useState([]);
-    const [vacanciesData, setVacanciesData] = useState({});
-    const [media, setMedia] = useState([]);
-    const [mainVideo, setMainVideo] = useState("");
+  const [contacts, setContacts] = useState({});
+  const [aboutUs, setAboutUs] = useState({});
+  const [services, setServices] = useState([]);
+  const [reviewsData, setReviewsData] = useState([]);
+  const [newsData, setNewsData] = useState([]);
+  const [vacanciesData, setVacanciesData] = useState({});
+  const [media, setMedia] = useState([]);
+  const [mainVideo, setMainVideo] = useState("");
 
-    async function fetchData() {
-        const responseContacts = await getContacts();
-        const responsePhones = await getPhones();
-        setContacts({
-            ...responseContacts.data,
-            "phones": responsePhones.data
-        })
+  async function fetchData() {
+    const responseContacts = await getContacts();
+    const responsePhones = await getPhones();
+    setContacts({
+      ...responseContacts.data,
+      "phones": responsePhones.data
+    })
 
-        await client.getCsrfToken();
+    await client.getCsrfToken();
 
-        const responseAboutUs = await getAboutUs();
-        setAboutUs({...responseAboutUs.data})
+    const responseAboutUs = await getAboutUs();
+    setAboutUs({ ...responseAboutUs.data })
 
-        const responseServices = await getServices();
-        setServices(responseServices.data);
+    const responseServices = await getServices();
+    setServices(responseServices.data);
 
-        const responseReviews = await getReviews();
-        setReviewsData(responseReviews.data);
+    const responseReviews = await getReviews();
+    setReviewsData(responseReviews.data);
 
-        const responseNews = await getNews();
-        setNewsData(responseNews.data);
+    const responseNews = await getNews();
+    setNewsData(responseNews.data);
 
-        const responseVacancies = await getVacancies();
-        setVacanciesData(responseVacancies.data);
+    const responseVacancies = await getVacancies();
+    setVacanciesData(responseVacancies.data);
 
-        const responseMediaImages = await getMediaImages();
-        const responseMediaVideo = await getMediaVideos();
-        setMainVideo(responseMediaVideo.data?.find(item => !!item?.is_main)?.video);
+    const responseMediaImages = await getMediaImages();
+    const responseMediaVideo = await getMediaVideos();
+    setMainVideo(responseMediaVideo.data?.find(item => !!item?.is_main)?.video);
 
-        setMedia(() => {
-            const mediaImages = responseMediaImages.data?.map(item => ({type: "image", "url": item?.image}));
-            const mediaVideos = responseMediaVideo.data?.map(item => ({type: "video", "url": item?.video}));
+    setMedia(() => {
+      const mediaImages = responseMediaImages.data?.map(item => ({ type: "image", "url": item?.image }));
+      const mediaVideos = responseMediaVideo.data?.map(item => ({ type: "video", "url": item?.video }));
 
-            return shuffle(mediaImages, mediaVideos);
-        })
-    }
+      return shuffle(mediaImages, mediaVideos);
+    })
+  }
 
-    useEffect(() => {
-        window.localStorage.setItem("lang", language);
-        setLoading(true);
-        fetchData().then(() => setLoading(false)).catch(() => setIsFetchError(true));
-    }, [language]);
+  useEffect(() => {
+    window.localStorage.setItem("lang", language);
+    setLoading(true);
+    fetchData().then(() => setLoading(false)).catch(() => setIsFetchError(true));
+  }, [language]);
 
-    return (
-        isFetchError ? <Spinner loading={isFetchError}/> : <div className="wrapper">
-            <Spinner loading={loading}/>
-            <LanguageContext.Provider value={[language, setLanguage]}>
-                <Header contacts={contacts}/>
-                <ScrollToTop/>
-                <Routes>
-                    <Route path="/*" element={<MainPage aboutUs={aboutUs}
-                                                        services={services}
-                                                        video={mainVideo}
-                                                        reviews={[reviewsData, loadingReviews]}
-                                                        news={[newsData, loadingNews]}
-                                                        media={media}/>}/>
-                    <Route path="/about-us/" element={<AboutUsPage aboutUs={aboutUs}/>}/>
-                    <Route path="/contacts/" element={<ContactsPage contacts={contacts}/>}/>
-                    <Route path="/services/" element={<ServicesPage services={services}/>}/>
-                    <Route path="/news/" element={<NewsListPage data={newsData}
-                                                                setData={setNewsData}
-                                                                loading={loadingNews}/>}/>
-                    <Route path="/news/:id/" element={<NewsDetailPage data={newsData}
-                                                                      loading={loadingNews}/>}/>
-                    <Route path="/vacancies/" element={<VacanciesListPage vacanciesData={vacanciesData}
-                                                                          setVacanciesData={setVacanciesData}
-                                                                          loadingVacanciesData={loadingVacancies}/>}/>
-                    <Route path="/vacancies/:id/" element={<VacancyDetailPage vacanciesData={vacanciesData}
-                                                                              loadingVacanciesData={loadingVacancies}/>}/>
-                    <Route path="/media/" element={<MediaPage data={media}/>}/>
-                </Routes>
-                <Footer contacts={contacts}/>
-            </LanguageContext.Provider>
-        </div>
+  return (
+    (isFetchError || loading) ? (
+      <Spinner loading={true} />
+    ) : (
+      <div className="wrapper">
+        <LanguageContext.Provider value={[language, setLanguage]}>
+          <Header contacts={contacts} />
+          <ScrollToTop />
+          <Routes>
+            <Route path="/*" element={<MainPage aboutUs={aboutUs}
+              services={services}
+              video={mainVideo}
+              reviews={[reviewsData, loadingReviews]}
+              news={[newsData, loadingNews]}
+              media={media} />} />
+            <Route path="/about-us/" element={<AboutUsPage aboutUs={aboutUs} />} />
+            <Route path="/contacts/" element={<ContactsPage contacts={contacts} />} />
+            <Route path="/services/" element={<ServicesPage services={services} />} />
+            <Route path="/news/" element={<NewsListPage data={newsData}
+              setData={setNewsData}
+              loading={loadingNews} />} />
+            <Route path="/news/:id/" element={<NewsDetailPage data={newsData}
+              loading={loadingNews} />} />
+            <Route path="/vacancies/" element={<VacanciesListPage vacanciesData={vacanciesData}
+              setVacanciesData={setVacanciesData}
+              loadingVacanciesData={loadingVacancies} />} />
+            <Route path="/vacancies/:id/" element={<VacancyDetailPage vacanciesData={vacanciesData}
+              loadingVacanciesData={loadingVacancies} />} />
+            <Route path="/media/" element={<MediaPage data={media} />} />
+          </Routes>
+          <Footer contacts={contacts} />
+        </LanguageContext.Provider>
+      </div>
     )
+  );
 }
 
 export default App;
